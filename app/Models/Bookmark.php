c7d2f472edbc5b6cffd69a39941f073759c553de
mahibashardeveloper <mahibashar2023@gmail.com>
2023-10-19 11:35:48 +0600

    update ui

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Bookmark extends Authenticatable
+class Bookmark extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -18,6 +18,7 @@ class Bookmark extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'bookmark_name',
         'bookmark_url',
     ];
 
